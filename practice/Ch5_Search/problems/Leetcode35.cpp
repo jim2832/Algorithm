d2a6dc7971a2cbee93257309d1875719712d99c2
jim2832 <jim880705@gmail.com>
2023-07-26 23:32:44 +0800

    230726 update

---
+++ practice/Ch5_Search/problems/Leetcode35.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        //recursion version
+        //return Binary_search(nums, 0, nums.size()-1, target);
+        
+        //iteration version
+        int lower = 0;
+        int upper = nums.size()-1;
+        int position;
+        while(upper >= lower){
+            int mid = (lower + upper) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                upper = mid-1;
+                position = mid;
+            }
+            else if(nums[mid] < target){
+                lower = mid+1;
+                position = mid+1;
+            }
+        }
+        return position;
+    }
+    
+    //recursion version
+    /*
+    int Binary_search(vector<int> &nums, int lower, int upper, int target){
+        if(upper < lower){
+            return lower;
+        }
+        
+        int mid = lower + (upper - lower) / 2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[mid] > target){
+            return Binary_search(nums, lower, mid-1, target);
+        }
+        else{
+            return Binary_search(nums, mid+1, upper, target);
+        }
+    }
+    */
+};
---
+++ practice/Ch5_Search/problems/Leetcode852.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        // for(int i=1; i<arr.size()-1; i++){
+        //     if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+        //         return i;
+        //     }
+        // }
+        
+        // return -1;
+        
+        int lower = 1;
+        int upper = arr.size()-2;
+        while(upper >= lower){
+            int mid = lower + (upper - lower) / 2;
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid] < arr[mid+1]){
+                lower = mid + 1;
+            }
+            else{
+                upper = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+};
