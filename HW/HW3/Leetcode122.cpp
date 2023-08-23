87a0201aa3690d17a18a1560f217d9f59f2ed309
Jim <jim880705@gmail.com>
2023-08-23 21:56:26 +0800

    230823 update

---
+++ HW/HW3/Leetcode122.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // 一次只能擁有一個股票
+        // 可以馬上買馬上賣
+        int maxProfit = 0;
+        
+        for(int i=0; i<prices.size()-1; i++){
+            maxProfit += max(prices[i+1] - prices[i], 0);
+        }
+   
+        return maxProfit;
+    }
+};
